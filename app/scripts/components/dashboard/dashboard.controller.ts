66d9f080c30e0622add5d86e834809d90ac416a3
Michael Kaaden <github@kaaden.net>
2018-03-29 14:14:20 +0200

    First steps to the reduxification of the dashboard

--- app/scripts/components/dashboard/dashboard.controller.ts
+++ app/scripts/components/dashboard/dashboard.controller.ts
@@ -1,3 +1,29 @@
+import * as ngRedux from "ng-redux";
+import { ICounterActionCreatorService } from "../../actions/counter.action-creator.service";
+import { IAppState } from "../../models/app-state";
+import { ICounter } from "../../models/counter.model";
+
 export class DashboardController {
     public static controllerId = "rdxDashboardController";
+    public static $inject = [
+        "$ngRedux",
+        "$scope",
+        "rdxCounterActionCreatorService"
+    ];
+
+    constructor(private $ngRedux: ngRedux.INgRedux,
+                private $scope: angular.IScope,
+                private counterActionCreatorService: ICounterActionCreatorService) {
+        const unsubscribe = this.$ngRedux.connect(this.mapStateToTarget.bind(this))(this);
+        this.$scope.$on("$destroy", unsubscribe);
+        this.counterActionCreatorService.loadAll();
+    }
+
+    private mapStateToTarget(state: IAppState) {
+        return {
+            counters: state.counters.all,
+            numOfCounters: state.counters.all.length,
+            counterValueSum: state.counters.all.reduce((accumulator: number, current: ICounter) => accumulator + current.value, 0)
+        };
+    }
 }
